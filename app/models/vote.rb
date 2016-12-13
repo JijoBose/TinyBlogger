a27ab5b3e6e995b549e88258cc06155427459b7c
JijoBose <bosejijo@gmail.com>
2016-12-13 20:02:22 +0530

    Blog with upvote

---
+++ app/models/vote.rb
@@ -0,0 +1,3 @@
+class Vote < ApplicationRecord
+  belongs_to :blog
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root 'blogs#index'
+  resources :blogs do
+    member do
+      post 'upvote'
+    end
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
