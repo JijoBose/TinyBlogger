9a0f34b3e78107b044e05675b84437acb23f00bb
Jijo Bose <bosejijo@gmail.com>
2017-02-11 20:36:21 +0530

    registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :blogs
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   get 'pages/index'
 
   get 'pages/about'
