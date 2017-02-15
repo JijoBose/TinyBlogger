676a41d761eeece62e541fe4194de859942ecd5b
Jijo Bose <bosejijo@gmail.com>
2017-02-15 13:58:42 +0530

    username to sign-up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,6 +5,7 @@ class RegistrationsController < Devise::RegistrationsController
   def sign_up_params
     params.require(:user).permit(
       :email,
+      :username,
       :password,
       :password_confirmation,
       :firstname,
@@ -15,6 +16,7 @@ class RegistrationsController < Devise::RegistrationsController
   def account_update_params
     params.require(:user).permit(
       :email,
+      :username,
       :password,
       :password_confirmation,
       :current_password,
